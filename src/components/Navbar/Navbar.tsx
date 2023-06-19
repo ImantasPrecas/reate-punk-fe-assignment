ba39086f2bea43fee4f1e847def73bca5b73bb6a
Imantas <precasimantas@gmail.com>
2023-06-19 15:33:19 +0300

    added mobile menu and hamburger button functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from 'react';
 import styles from '@/styles/navbar.module.scss';
 import Image from 'next/image';
 
@@ -6,60 +8,63 @@ import hamburgerBtnIcon from '@/assets/menu.svg';
 import closeBtn from '@/assets/close.svg';
 import Link from 'next/link';
 
+const MENU_LINKS = [
+	{ title: 'Chrome Extension', href: '/' },
+	{ title: 'Price Comparison', href: '/' },
+	{ title: 'Blog', href: '/' },
+];
+
 type Props = {};
 function Navbar({}: Props) {
+	const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+	const handleHamburgerClick = () => {
+		setIsNavExpanded(!isNavExpanded);
+	};
+
 	return (
 		<div className={styles.container}>
 			<nav className={styles.navbar}>
 				{/* MobileMenu */}
-				<div className={styles.mobile_menu}>
-					<div className={styles.mobile_actions}>
-						<Link href='/'>
-							<Image src={ratepunkLogo} height={32} alt='Brand Logo' />
-						</Link>
-						<button>
-							<Image src={closeBtn} alt='close menu button' />
-						</button>
+				{isNavExpanded && (
+					<div className={styles.mobile_menu}>
+						<div className={styles.mobile_actions}>
+							<Link href='/'>
+								<Image src={ratepunkLogo} height={32} alt='Brand Logo' />
+							</Link>
+							<button onClick={handleHamburgerClick}>
+								<Image src={closeBtn} alt='close menu button' />
+							</button>
+						</div>
+						<div className={styles.drop_menu_items}>
+							<ul>
+								{MENU_LINKS.map((link) => (
+									<li key={link.title}>
+										<Link href={link.href}>{link.title}</Link>
+									</li>
+								))}
+							</ul>
+						</div>
 					</div>
-					<div className={styles.drop_menu_items}>
-						<ul>
-							<li>
-								<Link href='/'>Chrome Extension</Link>
-							</li>
-							<li>
-								<Link href='/'>Price Comparison</Link>
-							</li>
-							<li>
-								<Link href='/'>Blog</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
+				)}
 				{/* /MobileMenu */}
 
-				<div>
+				<Link href='/'>
 					<Image src={ratepunkLogo} height={32} alt='Brand Logo' />
-				</div>
+				</Link>
 				<div className={styles.nav_links}>
 					<ul>
-						<li>
-							<Link href='/'>
-								<p>Chrome Extension</p>
-							</Link>
-						</li>
-						<li>
-							<Link href='/'>
-								<p>Price Comparison</p>
-							</Link>
-						</li>
-						<li>
-							<Link href='/'>
-								<p>Blog</p>
-							</Link>
-						</li>
+						{MENU_LINKS.map((link) => (
+							<li key={link.title}>
+								<Link href={link.href}>{link.title}</Link>
+							</li>
+						))}
 					</ul>
 				</div>
-				<button className={styles.hamburger_button}>
+				<button
+					onClick={handleHamburgerClick}
+					className={styles.hamburger_button}
+				>
 					<Image src={hamburgerBtnIcon} alt='hamburger menu' />
 				</button>
 			</nav>
